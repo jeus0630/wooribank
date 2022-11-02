acac5d85964288d32ffad55077a4093f1be3e43d
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2022-11-02 15:15:06 +0900

    add dependecies and remove useless code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h1 className="text-3xl bg-blue-500 text-green-300">Hello world!</h1>;
 }
 
-export default App
+export default App;
